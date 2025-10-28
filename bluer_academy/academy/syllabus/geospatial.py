e2f395af01b6ddc27097ae096146d2fc3d56d0d9
kamangir <arash@kamangir.net>
2025-10-28 21:37:34 +0330

    ++ - kamangir/bolt#746

--- bluer_academy/academy/syllabus/geospatial.py
+++ bluer_academy/academy/syllabus/geospatial.py
@@ -1,3 +1,5 @@
+from bluer_objects.README.consts import assets_url
+
 from bluer_academy.academy.syllabus.consts import bluer_geo_blob, bluer_geo_root
 from bluer_academy.academy.classes.topic import Topic
 
@@ -11,5 +13,9 @@ topic = Topic(
     ],
     duration=6,
     requires="python,bash,objects,cloud,plugins",
-    items={},
+    items={
+        assets_url(
+            suffix="blue-geo/chilcotin-query-2024-08-23-v1.png"
+        ): f"{bluer_geo_root}/catalog/copernicus"
+    },
 )
