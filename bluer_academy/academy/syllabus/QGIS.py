cfce1e55525ca610605b920c3671099f6c63dbc9
kamangir <arash@kamangir.net>
2025-10-28 16:54:27 +0330

    ++ - kamangir/bolt#746

--- bluer_academy/academy/syllabus/QGIS.py
+++ bluer_academy/academy/syllabus/QGIS.py
@@ -7,7 +7,7 @@ topic = Topic(
         "styling in QGIS",
         "templates in QGIS",
         "generating styled QGIS projects with algo",
-        "@QGIS",
+        "[@QGIS](https://github.com/kamangir/bluer-geo/blob/main/bluer_geo/QGIS/README.md)",
     ],
     duration=3,
     requires="geospatial",
