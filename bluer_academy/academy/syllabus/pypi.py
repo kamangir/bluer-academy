772e22ea02298b09a2f110965ed737ebbaa9ba74
kamangir <arash@kamangir.net>
2025-10-28 17:18:15 +0330

    ++ - kamangir/bolt#746

--- bluer_academy/academy/syllabus/pypi.py
+++ bluer_academy/academy/syllabus/pypi.py
@@ -3,7 +3,7 @@ from bluer_academy.academy.classes.topic import Topic
 topic = Topic(
     "pypi",
     [
-        "publishing code on pypi",
+        "[publishing code on pypi](https://github.com/kamangir/bluer-ai/tree/main/bluer_ai/.abcli/plugins/pypi)",
     ],
     duration=3,
     requires="linux,python,github,bash",
--- bluer_academy/docs/academy/pypi.md
+++ bluer_academy/docs/academy/pypi.md
@@ -1,7 +1,7 @@
 # academy: pypi
 
 includes:
-- publishing code on pypi.
+- [publishing code on pypi](https://github.com/kamangir/bluer-ai/tree/main/bluer_ai/.abcli/plugins/pypi).
 
 requires: [bash](./bash.md), [github](./github.md), [linux](./linux.md), [math](./math.md), [python](./python.md)
 
